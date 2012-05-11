14a7bb1fe34793abaa8932e2bb83493907b07c10
Thibault <thibaultfrancois1@gmail.com>
2012-05-11 04:28:21 +0200

    epuration de stack

--- stack.h
+++ stack.h
@@ -1,11 +1,16 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include "card.h"
+
 #include <QVector>
 #include <QPainter>
 
-#include "card.h"
-
+/*
+  Stack correspond aux 4 piles du haut à remplir
+  On ne peut les remplir qu'une d'une couleur
+  et les cartes se suivent dans l'ordre
+  */
 class Stack
 {
 public:
@@ -13,19 +18,22 @@ public:
     ~Stack();
 
     void addCard(Card*);
-    Card* getCardI(int) const;
-    inline Card* getRootCard() const { return cards;}
+
+    //Setters
     inline void setRootCard(Card* card_) {cards=card_;}
-
-
     inline void setPos(int x, int y) {posX=x; posY=y;}
     inline void setSize(int w_,int h_) {w = w_; h = h_;}
+
+    //Getters
+    Card* getCardI(int) const;
+    inline Card* getRootCard() const { return cards;}
     inline int &getX() {return posX;}
     inline int &getY() {return posY;}
     inline int &getW() {return w;}
     inline int &getH() {return h;}
-    void draw(QPainter &painter);
     int getSize();
+
+    void draw(QPainter &painter);
 
 private:
     Card* cards;
